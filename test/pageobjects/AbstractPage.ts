beba659927407532f925f021fd64f98b406108dc
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-22 11:55:17 -0700

    Add driver helpers

--- test/pageobjects/AbstractPage.ts
+++ test/pageobjects/AbstractPage.ts
@@ -1,9 +1,26 @@
 export default class AbstractPage {
     /**
      * Opens a sub page of the page
-     * @param path path of the sub page (e.g. /path/to/page.html)
+     * @param path - path of the sub page (e.g. /path/to/page.html)
      */
     public async open(path: string) {
         await browser.url(`/${path}`);
+    }
+
+    /**
+     * Set browser resolution
+     * @param width - width for new resolution
+     * @param height - height for new resolution
+     */
+    public async setResolution(width: number, height: number) {
+        await browser.setWindowSize(width, height);
+    }
+
+    /**
+     * Takes a screenshot of the screen
+     * @param path - path to save screenshot
+     */
+    public async takeScreenshot(path: string) {
+        await browser.saveScreenshot(path);
     }
 }
